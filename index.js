78661132432415c136f4297bf2791a4daee83816
Taki <laszlo@letmewebyou.com>
2021-03-15 22:48:09 +0100

    Added filter

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"))
 
 
@@ -27,10 +27,7 @@ const con = mysql.createConnection({
   database:process.env.DB
 })
 
-con.connect(function(err) {
-  if(err) throw err;
-  console.log("Connected to database!");
-})
+
 
 // MULTER UPLOAD
 var storage = multer.diskStorage({
@@ -50,84 +47,155 @@ const upload = multer({ storage: storage }).single('file')
 
 
 
-//Insert data from Form
-app.post("/api/distance", (req,res) => {
-  const today = new Date();
-  const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-  const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-  const currentDate = date +" "+time
-  const activity = req.body.activity_type
-
-  if (req.body.password != process.env.SECRETPASS) {
-  res.send("You have no permission to post data")
-} else {
-
-  if( activity === "Run" || activity === "Bike" || activity === "Roller Skates" ) {
-        const kilometers = parseInt(req.body.kilometers,10)
-        const sql = `INSERT INTO kmApp.done_distances (kilometers,steps, who, activity_type, date_created) VALUES ('${kilometers}','0','${req.body.who}','${req.body.activity_type}', '${currentDate}')`
-    con.query(sql, function (err,result) {
-      if (err) throw err;
-      res.json('Entry added for ' + `${req.body.activity_type}` +' !')
-      })
-  }
-
-  else if (activity === "Walk") {
-    const steps = parseInt(req.body.steps,10)
-    const kilometers = (steps*0.62/1000)
-    const sql = `INSERT INTO kmApp.done_distances (kilometers, steps, who, activity_type, date_created) VALUES ('${kilometers}','${steps}','${req.body.who}','${req.body.activity_type}', '${currentDate}')`
-    con.query( sql, function (err,result) {
-      if (err) throw err;
-      res.json("Entry added for Walk!")
-      })
-  }
-
-}
-
- })
+//GET REQUESTS//
 
 //Get all distance in SUM
-app.get("/api/getoveralldistance", (req,res) => {
+app.get("/api/getoveralldistance", async(req,res) => {
+  const email = req.query.email
+  const isBn = email.indexOf(process.env.BNEMAIL)
+  const isWs = email.indexOf (process.env.WSEMAIL)
+if(isBn > -1 || isWs > -1 ){
+  //If Email allowed
+  await con.connect(function(err) {
+    if(err) throw err;
+    console.log("Connected to database!");
+  });
   const sql = 'SELECT SUM(kilometers)+SUM((steps*0.62)/1000) AS doneDistance FROM done_distances'
   con.query( sql, function(err,result) {
+
     if (err) throw err;
     res.send(result[0].doneDistance)
-  })
+
+
+  }
+
+  )
+
+
+} else (res.json({
+  message: "You are not permitted to use this API"
+}))
+
+
+
 })
 
 //Get All distance without being filtered
 app.get('/api/getalldistance', (req,res) => {
-  const sql = 'SELECT * from done_distances'
-con.query(sql, function(err,result) {
-  if(err) throw err;
-  res.send(result);
+  const email = req.query.email
+  const isBn = email.indexOf(process.env.BNEMAIL)
+  const isWs = email.indexOf (process.env.WSEMAIL)
+  if(isBn > -1 || isWs > -1 ){
+    const sql = 'SELECT * from done_distances  ORDER BY 2 DESC'
+    con.query(sql, function(err,result) {
+      if(err) throw err;
+      res.send(result);
+
+    })
+  } else (res.json({
+    message: "You are not permitted to use this API"
+  }))
+
+
+
 
 })
-})
+
+// POST REQUESTS //
+
+//Insert data from Form
+app.post("/api/distance", async(req,res) => {
+  const email = req.body.email
+  const isBn = email.indexOf("@bitninja.io")
+  const isWs = email.indexOf ("@web-szerver.hu")
+  if(isBn > -1 || isWs > -1 ){
+
+    await con.connect(function(err) {
+      if(err) throw err;
+      console.log("Connected to database!");
+    });
+    const today = new Date();
+    const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    const currentDate = date +" "+time
+    const activity = req.body.activity_type
+
+    if( activity === "Run" || activity === "Bike" || activity === "Roller Skates" ) {
+          const kilometers = parseInt(req.body.kilometers,10)
+          const sql = `INSERT INTO kmApp.done_distances (kilometers,steps, who, activity_type, date_created) VALUES ('${kilometers}','0','${req.body.who}','${req.body.activity_type}', '${currentDate}')`
+      con.query(sql, function (err,result) {
+        if (err) throw err;
+        res.json('Entry added for ' + `${req.body.activity_type}` +' !')
+        })
+    }
+
+    else if (activity === "Walk") {
+      const steps = parseInt(req.body.steps,10)
+      const kilometers = (steps*0.62/1000)
+      const sql = `INSERT INTO kmApp.done_distances (kilometers, steps, who, activity_type, date_created) VALUES ('${kilometers}','${steps}','${req.body.who}','${req.body.activity_type}', '${currentDate}')`
+      con.query( sql, function (err,result) {
+        if (err) throw err;
+        res.json("Entry added for Walk!")
+        })
+    }
+  } else (res.json({
+    message: "You are not permitted to use this API"
+  }))
+
+
+
+
+
+
+
+ })
 
 app.post('/api/getuserdata', (req,res) => {
-  const user1 = req.body.user
-  const sql = `SELECT * FROM done_distances WHERE who = "${user1}"`
+  const email = req.body.email
+  const isBn = email.indexOf(process.env.BNEMAIL)
+  const isWs = email.indexOf (process.env.WSEMAIL)
+  if(isBn > -1 || isWs > -1 ){
+    const user1 = req.body.user
+    const sql = `SELECT * FROM done_distances WHERE who = "${user1}"`
 
-  con.query(sql, function(err,result){
-    if(err) throw(err);
-    res.send(result)
-    res.status(200);
-  })
+
+    con.query(sql, function(err,result){
+      if(err) throw(err);
+      res.send(result)
+      res.status(200);
+    })
+  } else (res.json({
+    message: "You are not permitted to use this API"
+  }))
+
+
+
+
 })
 
 
 //Upload picture from Form to img folder
 app.post('/api/upload', (req,res) => {
+  const email = req.query.email
+  const isBn = email.indexOf("@bitninja.io")
+  const isWs = email.indexOf ("@web-szerver.hu")
+  if(isBn > -1 || isWs > -1 ){
+    upload(req, res, function (err) {
 
-  upload(req, res, function (err) {
-    if (err instanceof multer.MulterError) {
-        return res.status(500).json(err)
-    } else if (err) {
-        return res.status(500).json(err)
-    }
-return res.status(200).send(req.file)
+      if (err instanceof multer.MulterError) {
+          return res.status(500).json(err)
+      } else if (err) {
+          return res.status(500).json(err)
+      }
 
-})
+  return res.status(200).send(req.file)
+
+  })
+  } else (res.json({
+    message: "You are not permitted to use this API"
+  }))
+
+
 })
 
 
